d8cf9878734715f633bc35b6f933792b1b1cdf2c
Abin Joeph <itsmeabjs@gmail.com>
2020-12-26 20:59:55 +0530

    Deply

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Route, Switch, HashRouter as Router} from 'react-router-dom'
 import App from './App';
 import { Provider } from './components/Login/helpers/Provider.js'
 import reducer, { initialState } from './components/Login/helpers/reducer'
+const routing = (
+    <Provider initialState={initialState} reducer={reducer}>
+
+    <Router>
+ 
+        <Switch>
+            <Route exact path="/" component ={App}/>
+            <Route path="/home" component ={App}/>
+        </Switch>
+
+    </Router>
+     </Provider>
+)
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider initialState={initialState} reducer={reducer}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
+    routing,
     document.getElementById('root')
 );
