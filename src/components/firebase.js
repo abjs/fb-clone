a2e83c2b45acd42a1d7a3aba299afafcf0c3ef65
Abin Joeph <itsmeabjs@gmail.com>
2020-12-27 10:42:17 +0530

    logo setUp

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,16 +1,16 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from 'firebase'  
 // import firebase from 'firebase/app'
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD9Gb7aUTwWzlv9kEWyDFkOeGUMfCzK_OE",
-  authDomain: "facebook-clone-77242.firebaseapp.com",
-  projectId: "facebook-clone-77242",
-  storageBucket: "facebook-clone-77242.appspot.com",
-  messagingSenderId: "89380552298",
-  appId: "1:89380552298:web:18df0ec245083c82175413",
-  measurementId: "G-L7GXGKBMWD"
-};
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain ,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket ,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
+  };
+
 const firebaseApp=firebase.initializeApp(firebaseConfig)
 const db=firebaseApp.firestore()
 const auth =firebase.auth()
